0c79b0a2d2513cb2b03b215c41ec4156db6a737f
Hpourtah <Hamoonptahmasbi@gmail.com>
2015-05-07 22:07:19 -0700

    Change Blue pin to Garage Icon

--- app/src/main/java/com/group_seven/csc413/finalprojectrepository/MapsActivity.java
+++ app/src/main/java/com/group_seven/csc413/finalprojectrepository/MapsActivity.java
@@ -3,14 +3,12 @@ package com.group_seven.csc413.finalprojectrepository;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -24,32 +22,28 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
-import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
-
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.ArrayList;
 
 
 public class MapsActivity extends ActionBarActivity implements OnMapLongClickListener, OnMarkerClickListener, View.OnClickListener {
@@ -653,7 +647,17 @@ public class MapsActivity extends ActionBarActivity implements OnMapLongClickLis
                 String address = oneObject.getString("DESC");
                 String[] garageLoc = temp.split(",");
                 LatLng garage = new LatLng(Double.parseDouble(garageLoc[1]), Double.parseDouble(garageLoc[0]));
-                mMap.addMarker(new MarkerOptions().position(garage).title(name + " - " + address).draggable(false).flat(true).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+
+                currentParkedMarker = mMap.addMarker(new MarkerOptions()
+                        .position(garage)
+                        .draggable(false)
+                        .title(name + " - " + address)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.garagesmall)));
+
+                currentParkedMarker.showInfoWindow();
+
+                //mMap.addMarker(new MarkerOptions().position(garage).title(name + " - " + address).draggable(false).flat(true).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
 
             } catch (JSONException e) {
                 // Oops
